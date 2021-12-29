e62734a8c8e5ebf25fdb7c96919440e9b7c57e9b
luwol03 <luwol03@gmail.com>
2021-12-29 16:34:42 +0100

    added i18n to menu template

--- src/electron/menu/index.js
+++ src/electron/menu/index.js
@@ -1,4 +1,5 @@
 const { shell, ipcMain } = require('electron');
+const { t } = require('i18next');
 
 const {
   DOCUMENTATION_URL,
@@ -32,22 +33,34 @@ const getMenuTemplate = () => {
           {
             label: 'Edit',
             submenu: [
-              { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
               {
-                label: 'Redo',
+                label: t('menu.edit.submenu.undo'),
+                accelerator: 'CmdOrCtrl+Z',
+                selector: 'undo:',
+              },
+              {
+                label: t('menu.edit.submenu.redo'),
                 accelerator: 'Shift+CmdOrCtrl+Z',
                 selector: 'redo:',
               },
               SEPARATOR_ENTRY(),
-              { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-              { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
               {
-                label: 'Paste',
+                label: t('menu.edit.submenu.cut'),
+                accelerator: 'CmdOrCtrl+X',
+                selector: 'cut:',
+              },
+              {
+                label: t('menu.edit.submenu.copy'),
+                accelerator: 'CmdOrCtrl+C',
+                selector: 'copy:',
+              },
+              {
+                label: t('menu.edit.submenu.paste'),
                 accelerator: 'CmdOrCtrl+V',
                 selector: 'paste:',
               },
               {
-                label: 'Select All',
+                label: t('menu.edit.submenu.selectAll'),
                 accelerator: 'CmdOrCtrl+A',
                 selector: 'selectAll:',
               },
@@ -56,30 +69,36 @@ const getMenuTemplate = () => {
         ]
       : []),
     {
-      label: 'View',
+      label: t('menu.view.label'),
       submenu: [
-        { role: 'reload' },
-        { role: 'forceReload' },
-        { role: 'toggleDevTools' },
+        { label: t('menu.view.submenu.reload'), role: 'reload' },
+        { label: t('menu.view.submenu.forceReload'), role: 'forceReload' },
+        {
+          label: t('menu.view.submenu.toggleDevTools'),
+          role: 'toggleDevTools',
+        },
         SEPARATOR_ENTRY(),
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
+        { label: t('menu.view.submenu.actualSize'), role: 'resetZoom' },
+        { label: t('menu.view.submenu.zoomIn'), role: 'zoomIn' },
+        { label: t('menu.view.submenu.zoomOut'), role: 'zoomOut' },
         SEPARATOR_ENTRY(),
-        { role: 'togglefullscreen' },
+        {
+          label: t('menu.view.submenu.toggleFullScreen'),
+          role: 'togglefullscreen',
+        },
       ],
     },
     {
-      label: 'Help',
+      label: t('menu.help.label'),
       submenu: [
-        LINK_ENTRY('Documentation', DOCUMENTATION_URL),
-        LINK_ENTRY('Learn More', ORGANIZATION_URL),
-        LINK_ENTRY('Community Discussions', DISCUSSION_URL),
-        LINK_ENTRY('Search Issues', ISSUES_URL),
+        LINK_ENTRY(t('menu.help.submenu.documentation'), DOCUMENTATION_URL),
+        LINK_ENTRY(t('menu.help.submenu.learnMore'), ORGANIZATION_URL),
+        LINK_ENTRY(t('menu.help.submenu.communityDiscussions'), DISCUSSION_URL),
+        LINK_ENTRY(t('menu.help.submenu.searchIssues'), ISSUES_URL),
         SEPARATOR_ENTRY(),
-        IPC_ENTRY('Check for updates', 'checkForUpdates'),
+        IPC_ENTRY(t('menu.help.submenu.checkForUpdates'), 'checkForUpdates'),
         SEPARATOR_ENTRY(),
-        IPC_ENTRY('About', 'openAbout'),
+        IPC_ENTRY(t('menu.help.submenu.about'), 'openAbout'),
       ],
     },
   ];
